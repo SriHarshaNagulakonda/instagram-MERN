e7f209847c294a8ea60ed0ed008625ad861d5d8e
SriHarshaNagulakonda <sriharshanagulakonda@gmail.com>
2021-04-14 07:42:18 +0530

    changed

--- server.js
+++ server.js
@@ -131,7 +131,7 @@ app.get('/api/syncComment',(req,res)=>{
 
 
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('cliend/build/'));
+    app.use(express.static('client/build/'));
     app.get('*',(req,res) => {
         res.sendFile(path.resolve(__dirname,'client','build','index.html'))
     })
